a60cca28c9f6217857ede346fb0d88c4cd921db6
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-17 19:49:08 -0400

    1

--- index.php
+++ index.php
@@ -5,9 +5,21 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=}, initial-scale=1.0">
   <title>Test de Chat Bot</title>
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="shortcut icon" href="assets/icon/chat-icon.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
-
+  <div class="wrapper">
+    <div class="title">Una Simple Prueba de un ChatBot Online</div>
+      <div class="bot-inbox inbox">
+        <div class="icon"><i class="fas fa-user"></i>
+      </div>
+      <div class="ms-header">
+        <p>Hello there, how i can help you?</p>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
