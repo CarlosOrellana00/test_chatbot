9da4d1147c85f1e1e3ea941d7c4d8af64d17a830
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-17 22:53:36 -0400

    6

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="shortcut icon" href="assets/icon/chat-icon.png">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
@@ -24,49 +25,21 @@
           </div>
         </div>
 
-        <div class="user-inbox inbox">
-          <div class="msg-header">
-            <p>Hi, i'm a message sender</p>
-          </div>
-        </div>
-        <!--  -->
-        <div class="bot-inbox inbox">
-          <div class="icon">
-            <i class="fas fa-user"></i>
-          </div>
-          <div class="msg-header">
-            <p>Hello there, how i can help you?</p>
-          </div>
-        </div>
-
-        <div class="user-inbox inbox">
-          <div class="msg-header">
-            <p>Hi, i'm a message sender</p>
-          </div>
-        </div>
-
-        <div class="bot-inbox inbox">
-          <div class="icon">
-            <i class="fas fa-user"></i>
-          </div>
-          <div class="msg-header">
-            <p>Hello there, how i can help you?</p>
-          </div>
-        </div>
-
-        <div class="user-inbox inbox">
-          <div class="msg-header">
-            <p>Hi, i'm a message sender</p>
-          </div>
-        </div>
-
     </div>
     <div class="typing-field">
       <div class="input-data">
-        <input type="text" placeholder="escribe algo aqui" required>
-        <button>Enviar</button>
+        <input id="data" type="text" placeholder="escribe algo aqui" required>
+        <button id="send-btn">Enviar</button>
       </div>
     </div>
   </div>
+  <script>
+    $(document).ready(function(){
+      $("#send-btn").on("click", function(){
+        $value = $("#data").val();
+        alert($value);
+      });
+    });
+  </script>
 </body>
 </html>
