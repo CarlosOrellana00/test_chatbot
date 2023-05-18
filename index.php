ec826fe674a6349f9cef17ab2c5621e8ea4225da
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-17 20:16:05 -0400

    2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=}, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Test de Chat Bot</title>
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="shortcut icon" href="assets/icon/chat-icon.png">
@@ -13,11 +13,19 @@
 <body>
   <div class="wrapper">
     <div class="title">Una Simple Prueba de un ChatBot Online</div>
-      <div class="bot-inbox inbox">
-        <div class="icon"><i class="fas fa-user"></i>
+      <div class="form">
+        <div class="bot-inbox inbox">
+          <div class="icon"><i class="fas fa-user"></i>
+        </div>
+        <div class="msg-header">
+          <p>Hello there, how i can help you?</p>
+        </div>
       </div>
-      <div class="ms-header">
-        <p>Hello there, how i can help you?</p>
+    </div>
+    <div class="typing-field">
+      <div class="input-data">
+        <input type="text" placeholder="escribe algo aqui">
+        <button>Enviar</button>
       </div>
     </div>
   </div>
